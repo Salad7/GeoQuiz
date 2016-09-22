a328d66453d07b00dc4d114f7a2e41dfa3e96e8a
Mohamed Salad <msalad@uncc.edu>
2016-09-22 01:02:41 -0400

    Completed

--- app/src/main/java/com/example/itsadmin/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/itsadmin/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.itsadmin.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "MainActivity";
+    private static final String KEY_INDEX = "index";
 
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
             new TrueFalse(R.string.question_oceans,true),
@@ -30,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        if(savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
         //Connecting Button objects to the proper view
         mFalseButton = (Button) findViewById(R.id.false_id);
         mTrueButton = (Button) findViewById(R.id.true_id);
@@ -86,5 +93,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstance)
+    {
+        super.onSaveInstanceState(savedInstance);
+        Log.i(TAG,"OnSaveInstanceState");
+        savedInstance.putInt(KEY_INDEX,mCurrentIndex);
     }
 }

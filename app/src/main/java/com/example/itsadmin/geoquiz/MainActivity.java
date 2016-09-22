0582901145b537c1c38000a7b886b886483caa77
Mohamed Salad <msalad@uncc.edu>
2016-09-21 23:26:07 -0400

    Updated Main
    -Imported Buttons
    -Set onClickListeners on said buttons

--- app/src/main/java/com/example/itsadmin/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/itsadmin/geoquiz/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.itsadmin.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Wiring up buttons
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //Connecting Button objects to the proper view
+        mFalseButton = (Button) findViewById(R.id.false_id);
+        mTrueButton = (Button) findViewById(R.id.true_id);
+
+        //Set up Listeners for the buttons
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Stuff
+            }
+        });
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Stuff
+            }
+        });
     }
 }

2e26bdf652eb5311471688305e715d8fe58599e5
Mohamed Salad <msalad@uncc.edu>
2016-09-25 00:01:43 -0400

    Manifest Update
    -Updated manifest to include cheat activity
    -Added onCreate(..) method tot Cheat activity

--- app/src/main/java/com/example/itsadmin/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/itsadmin/geoquiz/CheatActivity.java
@@ -1,7 +1,17 @@
 package com.example.itsadmin.geoquiz;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by itsadmin on 9/24/2016.
  */
-public class CheatActivity {
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle onSaveState)
+    {
+        super.onCreate(onSaveState);
+        setContentView(R.layout.activity_cheat);
+    }
 }
